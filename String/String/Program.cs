639fb7b652c346b8e1b58a48bda73e6cb61e4901
GDLinford <55755896+GDLinford@users.noreply.github.com>
2019-10-04 11:30:09 +0100

    Unfinished String Work

---
+++ String/String/Program.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//
+//namespace String
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string tempstring = "Hello it is a lovely day nice to meet you!";
+//
+//            string[] stringArray = tempstring.Split(' ');
+//
+//            for (int i = 0 ; i < stringArray.Length; i++){
+//                string s = string.Empty;
+//                for (int j = stringArray[i].Length - 1; j >=0 j--)
+//                {
+//                    s += stringArray[i][j];
+//                }
+//            }
+//        }
+//    }
+//}
